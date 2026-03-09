0d3a1ee3a6fc3791c53f45c0b887379380254574
ggrimace <leechseedsk@proton.me>
2026-03-09 08:16:32 -0400

    kjh

--- leechseed-explorer/src/main.jsx
+++ leechseed-explorer/src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import LeechseedExplorer from './leechseed_system_explorer'
+import BoldVentureExplorer from './bvx_system_explorer'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <LeechseedExplorer />
+    <BoldVentureExplorer />
   </StrictMode>
 )
